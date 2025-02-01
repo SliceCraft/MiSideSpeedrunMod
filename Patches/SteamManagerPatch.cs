302e08c6d49f2b8498eac8e81064c41df49d029d
Slice <51338223+SliceCraft@users.noreply.github.com>
2025-02-01 01:00:13 +0100

    Remove unused imports

--- Patches/SteamManagerPatch.cs
+++ Patches/SteamManagerPatch.cs
@@ -1,12 +1,4 @@
 ﻿using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BepInEx.Unity.IL2CPP.UnityEngine;
-using UnityEngine;
-using SpeedrunMod.RevealSystems;
 using SpeedrunMod.Toggles;
 
 namespace SpeedrunMod.Patches
--- Toggles/EnableRunToggle.cs
+++ Toggles/EnableRunToggle.cs
@@ -1,11 +1,4 @@
-﻿using SpeedrunMod.RevealSystems;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SpeedrunMod.Toggles
+﻿namespace SpeedrunMod.Toggles
 {
     internal class EnableRunToggle
     {
