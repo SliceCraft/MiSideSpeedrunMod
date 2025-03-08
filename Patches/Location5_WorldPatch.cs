338ae9e6dd703fc8aa6124e25debf6b219545396
Slice <51338223+SliceCraft@users.noreply.github.com>
2025-03-08 02:05:36 +0100

    Add fix for not getting an achievemt
    The creak in the dark achievement could be skipped accidentally when
    spamming the skip text button

---
+++ Patches/Location5_WorldPatch.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace SpeedrunMod.Patches;
+
+[HarmonyPatch(typeof(Location5_World))]
+// ReSharper disable once InconsistentNaming
+internal class Location5_WorldPatch
+{
+    private static AchievementsController _achievementsController = null;
+    
+    [HarmonyPatch(nameof(Location5_World.Update))]
+    [HarmonyPrefix]
+    // ReSharper disable once InconsistentNaming
+    private static void UpdatePatch(object[] __args)
+    {
+        if (_achievementsController == null)
+        {
+            _achievementsController = Object.FindObjectOfType<AchievementsController>();
+        }
+        if(_achievementsController == null) return;
+        foreach (DataAchievementsValues dataAchievement in _achievementsController.dataAchievements)
+        {
+            if (dataAchievement.steamAchievement.Equals("ACHI_ClickShadow"))
+            {
+                if (!dataAchievement.get)
+                {
+                    Plugin.Log.LogInfo("Creak in the Dark achievement obtained through mod");
+                    _achievementsController.AchievementCompleted("ACHI_ClickShadow");
+                }
+                break;
+            }
+        }
+    }
+}
