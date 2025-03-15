b9ff364d352d47933b9ab8c15a7c55a942acd9e7
Erik-Damian <erick.damian@encora.com>
2025-03-14 19:11:05 -0600

    Added buttons to the mod menu.

--- Menus/ModMenu.cs
+++ Menus/ModMenu.cs
@@ -1,7 +1,8 @@
 ﻿using MenuLib.API;
 using MenuLib.API.Factories;
 using SpeedrunMod.Menus.Practice;
-
+using SpeedrunMod.Utils;
+using UnityEngine;
 namespace SpeedrunMod.Menus;
 
 public static class ModMenu
@@ -9,12 +10,52 @@ public static class ModMenu
     public static void CreateMenu(GameMenu menu)
     {
         GameMenu practiceMenu = PracticeMenu.CreateMenu(menu);
-        
+
         new MenuOptionFactory()
             .SetName("PRACTICE")
             .SetParent(menu)
             .PlaceOptionBefore(menu.MenuOptions.Count - 1)
             .SetNextLocation(practiceMenu)
             .Build();
+        
+        new MenuOptionFactory()
+            .SetParent(menu)
+            .PlaceOptionBefore(menu.MenuOptions.Count - 1)
+            .BuildMenuDivider();
+        //The text inside the button could be changed to be shorter - Erik
+        if (!MyPluginInfo.PLUGIN_VERSION.Equals(VersionText.NewestVersion) && VersionText.NewestVersion != null)
+        {
+            new MenuOptionFactory()
+                .SetName("INSTALL LATEST VERSION FROM GITHUB")
+                .SetParent(menu)
+                .PlaceOptionBefore(menu.MenuOptions.Count - 1)
+                .SetNextLocation(menu)
+                .SetOnClick(OpenGithub)
+                .Build();
+        }
+        else
+        {
+            new MenuOptionFactory()
+                .SetName("GITHUB PAGE")
+                .SetParent(menu)
+                .PlaceOptionBefore(menu.MenuOptions.Count - 1)
+                .SetNextLocation(menu)
+                .SetOnClick(OpenGithub)
+                .Build();
+        }
+    }
+    
+    private static void OpenGithub()
+    {
+        if (!MyPluginInfo.PLUGIN_VERSION.Equals(VersionText.NewestVersion) && VersionText.NewestVersion != null)
+        {
+            Application.OpenURL(
+                url: $"https://github.com/SliceCraft/MiSideSpeedrunMod/releases/tag/{VersionText.NewestVersion}");
+        }
+        else
+        {
+            Application.OpenURL(
+                url: $"https://github.com/SliceCraft/MiSideSpeedrunMod");
+        }
     }
 }
