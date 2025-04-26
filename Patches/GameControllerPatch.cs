11caddba0f04c4b9f0bb58c94f55f1542b009d64
SliceCraft <51338223+SliceCraft@users.noreply.github.com>
2025-04-27 01:59:01 +0200

    Fix stuck on load after quitting practice mode
    Fixes #48
    The applied fix is described in the issue comments

---
+++ Patches/GameControllerPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using SpeedrunMod.Practice;
+
+namespace SpeedrunMod.Patches;
+
+[HarmonyPatch(typeof(GameController))]
+public class GameControllerPatch
+{
+    [HarmonyPatch(nameof(GameController.ExitGame))]
+    [HarmonyPrefix]
+    private static void ExitGamePatch()
+    {
+        PracticeManager.SelectedGame = PracticeGames.None;
+    }
+}
